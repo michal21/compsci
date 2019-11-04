75cae0e464b526e9df01a6aff0b9be6dd9e5ce10
michal21 <gancarczykm2121@gmail.com>
2019-11-04 11:27:11 +0100

    fix typo

--- sem5/sec/3/dhondt2011.py
+++ sem5/sec/3/dhondt2011.py
@@ -3,27 +3,41 @@ import csv
 
 
 def lastlost(seats, parties):
+    print("- Seats:", seats)
     parties = parties.copy()
     lastscr = 0
+    
     for p in parties:
         parties[p]['div'] = 1
+        parties[p]['chkseats'] = 0
+    
     while seats > -1:
         mx = 0
         mp = ''
+        
         for p in parties:
             if int(parties[p]['votes'] / parties[p]['div']) > mx:
                 mx = int(parties[p]['votes'] / parties[p]['div'])
                 mp = p
         seats -= 1
+        
+        if seats != -1:
+            parties[mp]['chkseats'] += 1
+
         if seats == 0:
             lastscr = int(parties[mp]['votes'] / parties[mp]['div'])
             print('Last: ', mp, ', scr: ', lastscr)
+        
         if seats == -1:
             lostscr = int(parties[mp]['votes'] / parties[mp]['div'])
             print('Lost: ', mp, ', scr: ', lostscr)
             print('Diff: ', (lastscr - lostscr) * parties[mp]['div'])
 
         parties[mp]['div'] += 1
+
+    for p in parties:
+        if 'seats' in parties[p] and parties[p]['seats'] != parties[p]['chkseats']:
+            print("Error for", p, ": seats =", parties[p]['seats'], ", chkseats =", parties[p]['chkseats'])
 
 
 total = {}
@@ -54,9 +68,9 @@ with open('2011.csv', 'r') as csvfile:
         if not 'seats' in total[name]:
             total[name]['seats'] = 0
 
-        if not 'votes' in total[name][party]:
+        if not 'votes' in sects[name][party]:
             sects[name][party]['votes'] = 0
-        if not 'seats' in total[name][party]:
+        if not 'seats' in sects[name][party]:
             sects[name][party]['seats'] = 0
         
         total[name]['votes'] += int(votes)
@@ -64,11 +78,14 @@ with open('2011.csv', 'r') as csvfile:
         if chosen == 'T':
             total[name]['seats'] += 1
             sects[name][party]['seats'] += 1
+            #print("Name:", name, ", Party:", party, ", Sum:", sects[name][party]['seats'])
 
 
 for sect in sects:
+    print("Sect:", sect)
     seats = total[sect]['seats']
     lastlost(seats, sects[sect])
+    print()
 
 #lastlost(8, {'A':{'votes':720},'B':{'votes':300},'C':{'votes':480}})
 
